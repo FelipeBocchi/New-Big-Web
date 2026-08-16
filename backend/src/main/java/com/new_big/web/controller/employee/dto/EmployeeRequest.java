c39b3a3f49efdf1310b7ab9e16db64372ee6dbc4
Felipe Bocchi <felipebocchibazzanella@gmail.com>
2026-08-16 14:28:41 -0300

    feat: creation of employee class

--- backend/src/main/java/com/new_big/web/controller/employee/dto/EmployeeRequest.java
+++ backend/src/main/java/com/new_big/web/controller/employee/dto/EmployeeRequest.java
@@ -1,44 +1,26 @@
-package com.new_big.web.entity;
+package com.new_big.web.controller.employee.dto;
 
 import com.new_big.web.enums.EmployeeRole;
-import jakarta.persistence.*;
-import lombok.AllArgsConstructor;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
 import lombok.Getter;
-import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 
-@Entity
 @Getter
 @Setter
-@NoArgsConstructor
-@AllArgsConstructor
-public class Employee {
-
-    @Id
-    @GeneratedValue
-    private Long id;
+public class EmployeeRequest {
 
     private String name;
-
     private String cpf;
-
     private String phone;
-
     private String email;
-
     private String username;
-
     private String password;
-
     private Boolean active;
-
     private Boolean admin;
-
-    private LocalDateTime createAt;
-
     private LocalDate birthDate;
 
     @Enumerated(EnumType.STRING)
---
+++ backend/src/main/java/com/new_big/web/repository/EmployeeRepository.java
@@ -0,0 +1,9 @@
+package com.new_big.web.repository;
+
+import com.new_big.web.entity.Employee;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface EmployeeRepository extends JpaRepository<Employee, Long> {
+}
