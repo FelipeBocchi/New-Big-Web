f87a3b9b257e65e04c67c4e4f4cacec956620c37
Felipe Bocchi <felipebocchibazzanella@gmail.com>
2026-09-15 20:12:05 -0300

    feat: ajuste do frontend para novo layout

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,26 +1,5 @@
 import { Routes } from '@angular/router';
-import { Login } from './login/login.component';
-import { FuncionariosComponent } from './funcionarios/funcionarios.component';
 
 export const routes: Routes = [
-  {
-    path: 'login',
-    component: Login
-  },
-
-  {
-    path: 'funcionarios',
-    component: FuncionariosComponent
-  },
-
-  {
-    path: '',
-    redirectTo: 'login',
-    pathMatch: 'full'
-  },
-
-  {
-    path: '**',
-    redirectTo: 'login'
-  }
+ 
 ];
---
+++ frontend/src/app/models/cashDailyLogs/cash-daily-log.spec.ts
@@ -0,0 +1,7 @@
+import { CashDailyLog } from './cash-daily-log';
+
+describe('CashDailyLog', () => {
+  it('should create an instance', () => {
+    expect(new CashDailyLog()).toBeTruthy();
+  });
+});
