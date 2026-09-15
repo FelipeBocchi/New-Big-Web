b29f44948c2d44148c9f146024ea7a21e35877e9
João <joaovictormelo43@gmail.com>
2026-09-15 20:18:07 -0300

    alterações

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,5 +1,5 @@
 import { Routes } from '@angular/router';
-import { Login } from './login/login.component';
+import { Login } from './layout/login/login.component';
 import { FuncionariosComponent } from './funcionarios/funcionarios.component';
 
 export const routes: Routes = [
