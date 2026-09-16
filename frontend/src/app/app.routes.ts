eace25076448a2bf8758c480c2d40c5bf44621d8
Felipe Bocchi <felipebocchibazzanella@gmail.com>
2026-09-16 20:35:10 -0300

    feat: criação do menu laral base para todas as telas

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,5 +1,17 @@
 import { Routes } from '@angular/router';
+import { LoginComponent } from './components/layout/login/login.component';
+import { PrincipalComponent } from './components/layout/principal/principal.component';
+import { CashMasterComponent } from './components/caixa/cash-master/cash-master.component';
 
 export const routes: Routes = [
- 
+
+    { path: "", redirectTo: "login", pathMatch: "full"},
+    { path: "login", component: LoginComponent},
+    {
+        path: "newBig", component: PrincipalComponent, children: [
+            { path: "cash", component: CashMasterComponent}
+            //aqui vai entrar os componentes de telas que tenham o menu lateral e topos os componentes em principal
+        ]
+    }
+
 ];
